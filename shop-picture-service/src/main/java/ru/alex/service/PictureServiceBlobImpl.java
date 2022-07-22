79b8764bfbba803e1c465ca3f3ffc251351ac404
Fox <gir_for@mail.ru>
2022-07-22 19:22:02 +0300

    Shop

---
+++ shop-picture-service/src/main/java/ru/alex/service/PictureServiceBlobImpl.java
@@ -0,0 +1,37 @@
+package ru.alex.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.alex.persist.model.Picture;
+import ru.alex.persist.model.PictureData;
+import ru.alex.persist.repo.PictureRepository;
+
+import java.util.Optional;
+
+@Service
+public class PictureServiceBlobImpl implements PictureService {
+
+    private final PictureRepository repository;
+
+    @Autowired
+    public PictureServiceBlobImpl(PictureRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Optional<String> getPictureContentTypeById(long id) {
+        return repository.findById(id)
+                .map(Picture::getContentType);
+    }
+
+    @Override
+    public Optional<byte[]> getPictureDataById(long id) {
+        return repository.findById(id)
+                .map(pic -> pic.getPictureData().getData());
+    }
+
+    @Override
+    public PictureData createPictureData(byte[] picture) {
+        return new PictureData(picture);
+    }
+}
